b4ddd2a57bc7396592db4cb94268ec891e40c628
PKDev51 <pragnakalp.dev51@gmail.com>
2024-09-24 13:04:21 +0530

    code added

---
+++ hello.py
@@ -0,0 +1,9 @@
+import requests
+
+url = "https://28d1-2401-4900-8899-e44d-d205-1008-1294-cfaf.ngrok-free.app/"
+data = {"data": "value"}
+
+response = requests.post(url, json=data)
+
+print(response.status_code)
+print(response.json())  # or response.text if the response is not JSON
